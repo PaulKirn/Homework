cca9511cff9ef7d9e7a877db5ea6f1b2a6252537
Paul <paul.kirn@yandex.ru>
2021-05-15 17:50:28 +0300

    Task 2 Date

--- SuperProject/Main.cpp
+++ SuperProject/Main.cpp
@@ -1,27 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
-#include <ctime>
+#include <time.h>
 
 using namespace std;
+
+int GetDay()
+{
+	struct tm newTime;
+	time_t now = time(0);
+	localtime_s(&newTime, &now);
+	return newTime.tm_mday;
+}
 
 int main()
 {
 
 	//Task 1
 
-	const int s = 4;
-	int array[s][s];
+	//const int s = 4;
+	//int array[s][s];
 
-	for(int i = 0; i < s; i++)
-	{
-		for (int j = 0; j < s; j++)
-		{
-			array[i][j] = i + j;
-			cout << array[i][j];
-		}
-		cout << '\n';
-	}
+	//for(int i = 0; i < s; i++)
+	//{
+	//	for (int j = 0; j < s; j++)
+	//	{
+	//		array[i][j] = i + j;
+	//		cout << array[i][j];
+	//	}
+	//	cout << '\n';
+	//}
 
+	//Task 2
 
-
+	cout << GetDay();
+	
 }
