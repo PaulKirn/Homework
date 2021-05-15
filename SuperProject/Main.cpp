800bbe7396147a27af937737d3cfb066c4dad505
Paul <paul.kirn@yandex.ru>
2021-05-15 21:10:21 +0300

    16.5 Homework solution

--- SuperProject/Main.cpp
+++ SuperProject/Main.cpp
@@ -17,21 +17,30 @@ int main()
 
 	//Task 1
 
-	//const int s = 4;
-	//int array[s][s];
+	const int s = 4;
+	int array[s][s];
 
-	//for(int i = 0; i < s; i++)
-	//{
-	//	for (int j = 0; j < s; j++)
-	//	{
-	//		array[i][j] = i + j;
-	//		cout << array[i][j];
-	//	}
-	//	cout << '\n';
-	//}
+	for(int i = 0; i < s; i++)
+	{
+		for (int j = 0; j < s; j++)
+		{
+			array[i][j] = i + j;
+			cout << array[i][j] << " ";
+		}
+		cout << '\n';
+	}
+	cout << '\n';
 
 	//Task 2
 
-	cout << GetDay();
+	int d = GetDay();
+	int a = d % s;
+
+	int sum = 0;
+	for (int i = 0; i < s; i++)
+	{
+		sum += array[a][i];
+	}
+	cout << "Sum of line items: " << sum << endl;
 	
 }
